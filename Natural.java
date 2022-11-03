fca8b244f112800bf7dda0a0b451467654bc51b8
yugaGit <yugarajbalamurugan.com>
2022-11-03 18:29:14 +0530

    first commit

---
+++ Natural.java
@@ -0,0 +1,11 @@
+class Natural{
+
+    public static void main(String[]ar){
+    int n=200;
+     
+  
+        for (int i=5;i<=n;i=i)
+        System.out.println(i+1);
+
+    }
+}
---
+++ bai.java
@@ -0,0 +1,10 @@
+public class bai{
+ 
+ public static void main(String[]args){
+  
+String a="Sadham hussain";
+ int age=22+10;
+ System.out.println("name="+a+"\n"+"age="+age);
+
+}
+}
